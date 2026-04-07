4b839f424669ac184514972cb87f017215249381
semen semenihin <pobeda.semenihin@gmail.com>
2026-04-07 12:08:39 +0700

    добавлена вьюшка для создания пользователя

---
+++ login/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import User
+class UserForm(forms.MdelForms):
+    class Meta:
+        model = User
+        fields = ["login","password","first_name","last_name","age"]
+        labels = {
+            "login": "Логин",
+            "password": "пароль",
+            "first_name": "имя",
+            "last_name": "фамилия",
+            "age": "возраст",
+        }
