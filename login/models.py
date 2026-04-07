c15f426b6fbcd6cd731ac4129d3ec202eac33a86
semen semenihin <pobeda.semenihin@gmail.com>
2026-04-07 12:14:35 +0700

    добавлен html шаблон для создания пользователя

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    login = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.PositiveIntegerField()
+    balance = models.FloatField(default=0.0)
